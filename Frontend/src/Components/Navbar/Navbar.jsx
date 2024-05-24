a74e8da4bc90e7487dd78f9821b48fd66851c8e2
arpitt45 <arpitkashyap903@gmail.com>
2024-05-24 23:59:51 +0530

    Add conponnts

---
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+       <div style={{background:"#f8faf5"}} className='w-4/5 h-10  '>
+           hemlo
+       </div>
+    </div>
+  )
+}
+
+export default Navbar
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,6 +5,11 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+
+<div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#b5e2c4_40%,#2a3958_100%)]">
+
+<App />
+</div>
+    
   </React.StrictMode>,
 )
